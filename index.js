5c07133bf0d2b69e4472c0191cc362ef3287a3e8
sdshuvo98 <hellosdshuvo@gamil.com>
2022-05-25 21:19:02 +0600

    delete parts by id

--- index.js
+++ index.js
@@ -199,6 +199,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/part/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await partCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
     } catch (error) {
         console.log(error);
